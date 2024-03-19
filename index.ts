ed27bc4823493e652a05cd5f5991df6df3fea06c
Matthias Van Overtvelt <vanovertveltmatthias@gmail.com>
2024-03-19 10:55:41 +0000

    work

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import * as readline from 'readline-sync';
 import { Player } from  "./interface";
 
 const app = express();
+const players: Player[] = JSON.parse(fs.readFileSync('players.json', 'utf-8'));
 
 app.set("view engine", "ejs"); // EJS als view engine
 app.set("port", 3000);
@@ -14,7 +15,9 @@ app.use(express.static('public'));
 app.set("port", 3000);
 
 app.get('/',(req,res)=>{
-            res.render('index')
+            res.render('index', {
+              players: players
+            })
         });
 
 app.listen(app.get("port"), () =>
